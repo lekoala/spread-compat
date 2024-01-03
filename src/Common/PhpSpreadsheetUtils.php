d1b0be77dc96737ea8b732e150b9246562afcbda
Thomas <thomas@lekoala.be>
2024-01-03 10:47:29 +0100

    remove duplicated code

---
+++ src/Common/PhpSpreadsheetUtils.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LeKoala\SpreadCompat\Common;
+
+use Exception;
+use Generator;
+use LeKoala\SpreadCompat\SpreadCompat;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\BaseReader;
+use PhpOffice\PhpSpreadsheet\Writer\BaseWriter;
+
+trait PhpSpreadsheetUtils
+{
+    protected function getReaderClass(): string
+    {
+        throw new Exception("Method not implemented");
+    }
+
+    protected function getWriterClass(): string
+    {
+        throw new Exception("Method not implemented");
+    }
+
+    protected function getMimetype(): string
+    {
+        throw new Exception("Method not implemented");
+    }
+
+    protected function getReader(): BaseReader
+    {
+        $class = $this->getReaderClass();
+        /** @var \PhpOffice\PhpSpreadsheet\Reader\Xls|\PhpOffice\PhpSpreadsheet\Reader\Xlsx $reader */
+        $reader = new ($class);
+        // We are only interested in cell data
+        $reader->setReadDataOnly(true);
+        return $reader;
+    }
+
+    protected function readSpreadsheet(Spreadsheet $spreadsheet): Generator
+    {
+        $headers = null;
+        foreach ($spreadsheet->getActiveSheet()->getRowIterator() as $row) {
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(false);
+            $data = [];
+            foreach ($cellIterator as $cell) {
+                $v = $cell->getValue();
+                $data[] = $v;
+            }
+            if (empty($data) || $data[0] === null) {
+                continue;
+            }
+            if ($this->assoc) {
+                if ($headers === null) {
+                    $headers = $data;
+                    continue;
+                }
+                $data = array_combine($headers, $data);
+            }
+            yield $data;
+        }
+    }
+
+    public function readString(
+        string $contents,
+        ...$opts
+    ): Generator {
+        $filename = SpreadCompat::getTempFilename();
+        file_put_contents($filename, $contents);
+        yield from $this->readFile($filename, ...$opts);
+        unlink($filename);
+    }
+
+    public function readFile(
+        string $filename,
+        ...$opts
+    ): Generator {
+        $this->configure(...$opts);
+        $spreadsheet = $this->getReader()->load($filename);
+        yield from $this->readSpreadsheet($spreadsheet);
+    }
+
+    protected function getWriter(iterable $source): BaseWriter
+    {
+        $spreadsheet = new Spreadsheet();
+        if (!is_array($source)) {
+            $source = iterator_to_array($source);
+        }
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->fromArray($source);
+        if ($this->autofilter) {
+            $sheet->setAutoFilter($this->autofilter);
+        }
+        if ($this->freezePane) {
+            $sheet->freezePane($this->freezePane);
+        }
+        $class = $this->getWriterClass();
+         /** @var \PhpOffice\PhpSpreadsheet\Writer\Xls|\PhpOffice\PhpSpreadsheet\Writer\Xlsx $writer */
+        $writer = new ($class)($spreadsheet);
+        return $writer;
+    }
+
+    public function writeString(iterable $data, ...$opts): string
+    {
+        $filename = SpreadCompat::getTempFilename();
+        $this->writeFile($data, $filename);
+        $contents = file_get_contents($filename);
+        if (!$contents) {
+            $contents = "";
+        }
+        unlink($filename);
+        return $contents;
+    }
+
+    public function writeFile(iterable $data, string $filename, ...$opts): bool
+    {
+        $this->configure(...$opts);
+        $writer = $this->getWriter($data);
+        $writer->save($filename);
+        return true;
+    }
+
+    public function output(iterable $data, string $filename, ...$opts): void
+    {
+        $this->configure(...$opts);
+        $writer = $this->getWriter($data);
+
+        header('Content-Type: ' . $this->getMimetype());
+        header(
+            'Content-Disposition: attachment; ' .
+                'filename="' . rawurlencode($filename) . '"; ' .
+                'filename*=UTF-8\'\'' . rawurlencode($filename)
+        );
+        header('Cache-Control: max-age=0');
+        header('Pragma: public');
+
+        ob_end_clean();
+        ob_start();
+        $writer->save('php://output');
+    }
+}
--- src/Xls/PhpSpreadsheet.php
+++ src/Xls/PhpSpreadsheet.php
@@ -4,121 +4,26 @@ declare(strict_types=1);
 
 namespace LeKoala\SpreadCompat\Xls;
 
-use Generator;
-use RuntimeException;
-use LeKoala\SpreadCompat\SpreadCompat;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use LeKoala\SpreadCompat\Common\PhpSpreadsheetUtils;
 use PhpOffice\PhpSpreadsheet\Reader\Xls as ReaderXls;
 use PhpOffice\PhpSpreadsheet\Writer\Xls as WriterXls;
 
 class PhpSpreadsheet extends XlsAdapter
 {
-    protected function getReader(): ReaderXls
+    use PhpSpreadsheetUtils;
+
+    protected function getReaderClass(): string
     {
-        $reader = new ReaderXls();
-        // We are only interested in cell data
-        $reader->setReadDataOnly(true);
-        return $reader;
+        return ReaderXls::class;
     }
 
-    protected function readSpreadsheet(Spreadsheet $spreadsheet): Generator
+    protected function getWriterClass(): string
     {
-        $headers = null;
-        foreach ($spreadsheet->getActiveSheet()->getRowIterator() as $row) {
-            $cellIterator = $row->getCellIterator();
-            $cellIterator->setIterateOnlyExistingCells(false);
-            $data = [];
-            foreach ($cellIterator as $cell) {
-                $v = $cell->getValue();
-                $data[] = $v;
-            }
-            if (empty($data) || $data[0] === null) {
-                continue;
-            }
-            if ($this->assoc) {
-                if ($headers === null) {
-                    $headers = $data;
-                    continue;
-                }
-                $data = array_combine($headers, $data);
-            }
-            yield $data;
-        }
+        return WriterXls::class;
     }
 
-    public function readString(
-        string $contents,
-        ...$opts
-    ): Generator {
-        $filename = SpreadCompat::getTempFilename();
-        file_put_contents($filename, $contents);
-        yield from $this->readFile($filename);
-        unlink($filename);
-    }
-
-    public function readFile(
-        string $filename,
-        ...$opts
-    ): Generator {
-        $this->configure(...$opts);
-        $spreadsheet = $this->getReader()->load($filename);
-        yield from $this->readSpreadsheet($spreadsheet);
-    }
-
-    protected function getWriter(iterable $source): WriterXls
+    protected function getMimetype(): string
     {
-        $spreadsheet = new Spreadsheet();
-        if (!is_array($source)) {
-            $source = iterator_to_array($source);
-        }
-        $sheet = $spreadsheet->getActiveSheet();
-        $sheet->fromArray($source);
-        if ($this->autofilter) {
-            $sheet->setAutoFilter($this->autofilter);
-        }
-        if ($this->freezePane) {
-            $sheet->freezePane($this->freezePane);
-        }
-        $writer = new WriterXls($spreadsheet);
-        return $writer;
-    }
-
-    public function writeString(iterable $data, ...$opts): string
-    {
-        $filename = SpreadCompat::getTempFilename();
-        $this->writeFile($data, $filename);
-        $contents = file_get_contents($filename);
-        if (!$contents) {
-            $contents = "";
-        }
-        unlink($filename);
-        return $contents;
-    }
-
-    public function writeFile(iterable $data, string $filename, ...$opts): bool
-    {
-        $this->configure(...$opts);
-        $writer = $this->getWriter($data);
-        $writer->save($filename);
-        return true;
-    }
-
-    public function output(iterable $data, string $filename, ...$opts): void
-    {
-        $this->configure(...$opts);
-        $writer = $this->getWriter($data);
-
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header(
-            'Content-Disposition: attachment; ' .
-                'filename="' . rawurlencode($filename) . '"; ' .
-                'filename*=UTF-8\'\'' . rawurlencode($filename)
-        );
-        header('Cache-Control: max-age=0');
-        header('Pragma: public');
-
-        ob_end_clean();
-        ob_start();
-        $writer->save('php://output');
+        return 'application/vnd.ms-excel';
     }
 }
